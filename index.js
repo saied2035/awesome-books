50b2a96e37c8cacbf4f5d82c7543bfbd3df43292
unknown <saied2421998@gmail.com>
2022-02-21 11:53:28 +0200

    add add-book&contact sections & create book-list class

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ import {DateTime} from './modules/luxon/src/luxon.js'
 const date = document.querySelector('#date');
 date.innerText = DateTime.now().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS)
 
+import BookList from './modules/Book-list.js'
+const listOfBooks = new BookList();
---
+++ modules/book-list.js
@@ -0,0 +1,36 @@
+export default class BookList {
+  constructor() {
+    this.bookList = [];
+  }
+
+  addBook(title = '', author = '') {
+    const list = document.querySelector('.booklist');
+    list.style.padding = '0';
+    const li = document.createElement('li');
+    li.className = 'book-item';
+    li.style.margin = '0';
+    const bookTitle = document.createElement('p');
+    bookTitle.className = 'title';
+    bookTitle.innerText = `"${title}" by`;
+    const bookAuthor = document.createElement('p');
+    bookAuthor.className = 'author';
+    bookAuthor.innerText = author;
+    const removeBtn = document.createElement('button');
+    removeBtn.className = 'remove';
+    removeBtn.innerText = 'Remove';
+    li.appendChild(bookTitle);
+    li.appendChild(bookAuthor);
+    li.appendChild(removeBtn);
+    list.appendChild(li);
+    const book = { title, author };
+    this.bookList.push(book);
+    window.localStorage.setItem('bookList', JSON.stringify(this.bookList));
+  }
+
+  removeBook(index) {
+    this.bookList = this.bookList.filter((book, i) => this.bookList[index] !== this.bookList[i]);
+    window.localStorage.setItem('bookList', JSON.stringify(this.bookList));
+  }
+}
+
+const listOfBooks = new BookList();
